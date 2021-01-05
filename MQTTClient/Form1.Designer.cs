e9dc21c7c122559001a5f3c3da47b6eb01f74e0c
g.pansegrauw <g.pansegrauw@yellotec.com>
2021-01-05 08:09:40 +0200

    Add project files.

---
+++ MQTTClient/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace MQTTClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnUbsubscribe = new System.Windows.Forms.Button();
+            this.txtInfo = new System.Windows.Forms.TextBox();
+            this.btnSubscribe = new System.Windows.Forms.Button();
+            this.txtTopicSubscribe = new System.Windows.Forms.TextBox();
+            this.txtReceived = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnUbsubscribe
+            // 
+            this.btnUbsubscribe.Location = new System.Drawing.Point(713, 12);
+            this.btnUbsubscribe.Name = "btnUbsubscribe";
+            this.btnUbsubscribe.Size = new System.Drawing.Size(75, 23);
+            this.btnUbsubscribe.TabIndex = 1;
+            this.btnUbsubscribe.Text = "Unsubscribe";
+            this.btnUbsubscribe.UseVisualStyleBackColor = true;
+            this.btnUbsubscribe.Click += new System.EventHandler(this.btnUbsubscribe_Click);
+            // 
+            // txtInfo
+            // 
+            this.txtInfo.Location = new System.Drawing.Point(12, 390);
+            this.txtInfo.Multiline = true;
+            this.txtInfo.Name = "txtInfo";
+            this.txtInfo.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtInfo.Size = new System.Drawing.Size(776, 48);
+            this.txtInfo.TabIndex = 2;
+            // 
+            // btnSubscribe
+            // 
+            this.btnSubscribe.Location = new System.Drawing.Point(632, 12);
+            this.btnSubscribe.Name = "btnSubscribe";
+            this.btnSubscribe.Size = new System.Drawing.Size(75, 23);
+            this.btnSubscribe.TabIndex = 3;
+            this.btnSubscribe.Text = "Subscribe";
+            this.btnSubscribe.UseVisualStyleBackColor = true;
+            this.btnSubscribe.Click += new System.EventHandler(this.btnSubscribe_Click);
+            // 
+            // txtTopicSubscribe
+            // 
+            this.txtTopicSubscribe.Location = new System.Drawing.Point(12, 12);
+            this.txtTopicSubscribe.Name = "txtTopicSubscribe";
+            this.txtTopicSubscribe.Size = new System.Drawing.Size(614, 20);
+            this.txtTopicSubscribe.TabIndex = 4;
+            this.txtTopicSubscribe.Text = "vgdc/9524410/status";
+            // 
+            // txtReceived
+            // 
+            this.txtReceived.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtReceived.Location = new System.Drawing.Point(12, 41);
+            this.txtReceived.Multiline = true;
+            this.txtReceived.Name = "txtReceived";
+            this.txtReceived.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtReceived.Size = new System.Drawing.Size(776, 340);
+            this.txtReceived.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtReceived);
+            this.Controls.Add(this.txtTopicSubscribe);
+            this.Controls.Add(this.btnSubscribe);
+            this.Controls.Add(this.txtInfo);
+            this.Controls.Add(this.btnUbsubscribe);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "MQTT Client";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnUbsubscribe;
+        private System.Windows.Forms.TextBox txtInfo;
+        private System.Windows.Forms.Button btnSubscribe;
+        private System.Windows.Forms.TextBox txtTopicSubscribe;
+        private System.Windows.Forms.TextBox txtReceived;
+    }
+}
+
---
+++ MQTTClient/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+ // including the M2Mqtt Library
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+namespace MQTTClient
+{
+    public partial class Form1 : Form
+    {
+
+        string connectionString;
+
+        string sqlUsername;
+        string sqlPassword;
+        string sqlServerName;
+        string sqlDatabase;
+        string sqlConnectionString;
+        //MqttClient client = new MqttClient(IPAddress.Parse("192.168.10.53"));
+        MqttClient client;
+        string clientId;
+
+        public Form1()
+        {
+            InitializeComponent();
+            string BrokerAddress = "52.157.232.196";
+
+            client = new MqttClient(BrokerAddress);
+
+            // register a callback-function (we have to implement, see below) which is called by the library when a message was received
+            client.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
+
+            // use a unique id as client id, each time we start the application
+            clientId = Guid.NewGuid().ToString();
+
+            client.Connect(clientId);
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            client.Disconnect();
+
+            base.OnClosed(e);
+           //Application.Current.Shutdown();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        
+        void client_MqttMsgUnsubscribed(object sender, MqttMsgUnsubscribedEventArgs e)
+        {
+            // write your code
+        }
+
+        void client_MqttMsgSubscribed(object sender, MqttMsgSubscribedEventArgs e)
+        {
+            MessageBox.Show(e.ToString());
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+      
+        }
+
+        private void btnUbsubscribe_Click(object sender, EventArgs e)
+        {
+            string[] topics = { "sensor/temp", "sensor/humidity" };
+
+            client.Unsubscribe(topics);
+        }
+
+        public void toSQL(string message, string topic)
+        {
+            sqlDatabase = "OMNITREND_TOOLS";//Properties.Settings.Default.sqlDatabase.ToString();
+            sqlPassword = "Yello100";//Properties.Settings.Default.sqlPassword.ToString();
+            sqlServerName = "ytsdtsql";//Properties.Settings.Default.sqlServerName.ToString();
+            sqlUsername = "sa";//Properties.Settings.Default.sqlUsername.ToString();
+
+            string connString = "user id=" + sqlUsername + ";password=" + sqlPassword + "; server=" + sqlServerName + "; database= " + sqlDatabase + ";connection timeout=30";
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connString))
+                {
+                    String query = "INSERT INTO [MQTT] ([topic], [message]) VALUES (@val1, @val2)";
+
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+
+                        command.Parameters.AddWithValue("@val1", topic);
+                        command.Parameters.AddWithValue("@val2", message);
+
+                        connection.Open();
+
+                        int result = command.ExecuteNonQuery();
+                        connection.Close();
+
+                        // Check Error
+                        if (result < 0)
+                            MessageBox.Show("Could not add to database", "There was a problem.",
+                                MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        else
+                        {
+                            //MessageBox.Show("Added");
+                        }
+                    }
+                }
+            }
+            catch (Exception exe)
+            {
+                MessageBox.Show("Could not add the finding data DETAILS - " + exe.Message, "There was a problem.",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnSubscribe_Click(object sender, EventArgs e)
+        {
+            if (txtTopicSubscribe.Text != "")
+            {
+                // whole topic
+                string Topic = txtTopicSubscribe.Text;
+
+                // subscribe to the topic with QoS 1
+                client.Subscribe(new string[] { Topic }, new byte[] { 1 });   // we need arrays as parameters because we can subscribe to different topics with one call
+              
+                 txtReceived.Text = "";
+            }
+            else
+            {
+                txtInfo.Text = txtInfo.Text + "You have to enter a topic to subscribe!/r/n";
+            }
+        }
+
+        // this code runs when a message was received
+        void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+        {
+            string ReceivedMessage = Encoding.UTF8.GetString(e.Message);
+          //  string ReceivedTopic = Encoding.UTF8.GetString(e.Topic);
+
+
+            txtReceived.Invoke((MethodInvoker)(() => txtReceived.Text = ReceivedMessage));
+            toSQL(ReceivedMessage, e.Topic.ToString());
+
+
+        }
+
+    }
+}
